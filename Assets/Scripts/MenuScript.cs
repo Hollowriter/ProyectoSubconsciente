d1bfcdc5deb26b028340133ea750a9492160587b
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-08-19 22:55:37 -0300

    movimiento por tiles en proceso

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuScript // Como no va a estar agregado en ningun objeto, no tiene porque ser monobehaviour
+{
+    [MenuItem("Tools/Assign_Tile_Material")]
+    public static void AssignTileMaterial()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Tile");
+        foreach (GameObject t in tiles)
+        {
+            t.GetComponent<Renderer>().material = material;
+        }
+    }
+   [MenuItem("Tools/Assign_Tile_Script")]
+    public static void AssignTileScript()
+    {
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (GameObject t in tiles)
+        {
+            t.AddComponent<Tile>();
+        }
+    }
+}
