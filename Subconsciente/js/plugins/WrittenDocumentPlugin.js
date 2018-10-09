3b6d3d7768abaed82ece69907a72829def9d5cf4
Hollowriter <deadgamergx2@hotmail.com.ar>
2018-10-09 16:10:28 -0300

    haciendo un plugin con node.js para escribir texto

---
+++ Subconsciente/js/plugins/WrittenDocumentPlugin.js
@@ -0,0 +1,40 @@
+// ===========================================================================
+//	WrittenDocumentPlugin.js
+// ===========================================================================
+(function(){
+
+	function setup(){
+		function WrittenDocumentFS(){
+
+		}
+
+		WrittenDocumentFS.fs = require("fs");
+
+		WrittenDocumentFS.writeFile = function (filePath, fileName, data){
+			filePath = this.createPath("/" + filePath + "/");
+			this.fs.writeFileSync(filePath + fileName, data);
+			console.log("Wrote file: " + fileName);
+		};
+
+		WrittenDocumentFS.readFile = function(filePath, fileName){
+			filePath = this.createPath("/" + filePath + "/");
+			console.log("Read File: ", fileName);
+			return this.fs.readFileSync(filePath + fileName, "utf8");
+		};
+
+		WrittenDocumentFS.createPath = function (thyPath){
+			thyPath = (Utils.isNwjs() && Utils.isOptionValid("test")) ? thyPath : "/www/" + thyPath;
+			var path = window.location.pathname.replace(/(\/www|)\/[^\/]*$/, thyPath);
+			if (path.match(/^\/([A-Z]\:)/)){
+				path = path.slice(1);
+			}
+			path = decodeURIComponent(path);
+			return path;
+		};
+
+		window.WrittenDocumentFS = WrittenDocumentFS;
+	}
+
+	setup();
+
+})();
